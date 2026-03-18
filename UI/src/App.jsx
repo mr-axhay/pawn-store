75a4ea69c7c342fc92582bb4eaa6b8c4bdfc2b18
mr-axhay <mr.axhay@gmail.com>
2026-03-19 02:20:50 +0530

    fix import again

--- UI/src/App.jsx
+++ UI/src/App.jsx
@@ -20,6 +20,7 @@ import { useLocation } from "react-router-dom";
 import SubAdminHomePage from "./components/SubAdminHomePage";
 import OpenAI from "./components/openAI";
 
+
 function App() {
   const location = useLocation();
 
